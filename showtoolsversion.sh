7d6e1d54dc15653e26cc7e61cd7cd4c8ce0545ef
quizforparty44 <quizforparty44@gmail.com>
2019-04-05 15:28:09 +0200

    show tools version

---
+++ showtoolsversion.sh
@@ -0,0 +1,25 @@
+#Oracle Instant Client
+sqlplus -v
+imp help=Y
+exp help=Y
+impdp help=Y
+expdp help=Y
+#Usefull Tools
+jq --version
+unzip -v
+curl --version
+wget --version
+#Terraform
+terraform --version
+#OCI CLI
+oci --version
+#Java
+java -version
+#Sqlcl
+exit | sql /nolog
+#Vault
+vault --version
+#kubectl
+kubectl --help
+#helm
+helm --help
